29d29580fb20e7299b648c9b5e29da195c2d4ada
kaiqiw <1132620342@qq.com>
2018-10-10 17:18:41 +0800

    first commit

---
+++ pages/about/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeIndex2: '1'
+    }
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,70 @@
+<template>
+  <section class="container">
+    <div>
+      <el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" @select="handleSelect" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+        <el-menu-item index="1">
+          <nuxt-link to="/work">商业合作</nuxt-link>
+        </el-menu-item>
+
+        <el-submenu index="2">
+          <template slot="title">
+            <nuxt-link to="/doc">开发接入</nuxt-link>
+          </template>
+          <el-menu-item index="2-1">选项1</el-menu-item>
+          <el-menu-item index="2-2">选项2</el-menu-item>
+          <el-menu-item index="2-3">选项3</el-menu-item>
+          <el-submenu index="2-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="2-4-1">选项1</el-menu-item>
+            <el-menu-item index="2-4-2">选项2</el-menu-item>
+            <el-menu-item index="2-4-3">选项3</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+
+        <el-menu-item index="3">
+          <nuxt-link to="/doc">文档中心</nuxt-link>
+        </el-menu-item>
+
+        <el-menu-item index="4">
+          <nuxt-link to="/about">关于我们</nuxt-link>
+        </el-menu-item>
+      </el-menu>
+
+      <div class="block">
+        <el-carousel height="500px">
+          <el-carousel-item v-for="item in 4" :key="item">
+            <h3>{{ item }}</h3>
+          </el-carousel-item>
+        </el-carousel>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
